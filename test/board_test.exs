216c78b5d77e817bdbed98533ad825afb2b770a3
Camilo Cunha de Azevedo <camilotk@gmail.com>
2025-10-21 19:38:08 -0300

    fixed: revenge rule

--- test/board_test.exs
+++ test/board_test.exs
@@ -1,62 +1,63 @@
 defmodule BoardTest do
-	use ExUnit.Case
-	alias Board
+  use ExUnit.Case
+  alias Board
 
   describe "new/0" do
     test "creates a new empty board" do
       assert Board.new() == %Board{
         cells: [
-        " ", " ", " "," ",
-        " ", " ", " "," ",
-        " ", " ", " "," ",
-        " ", " ", " "," ",
-      ]
-    }
+          " ", " ", " ", " ",
+          " ", " ", " ", " ",
+          " ", " ", " ", " ",
+          " ", " ", " ", " "
+        ],
+        prev_owner: %{}
+      }
     end
   end
 
   describe "play" do
     test "play once" do
-      new_board = Board.new()
+      new_board =
+        Board.new()
         |> Board.play(Player.new(:x, 1), 1)
 
-      assert new_board == %Board{
-        cells: [
-          :x, " ", " "," ",
-          " ", " ", " "," ",
-          " ", " ", " "," ",
-          " ", " ", " "," ",
+      assert new_board.cells == [
+        :x, " ", " ", " ",
+        " ", " ", " ", " ",
+        " ", " ", " ", " ",
+        " ", " ", " ", " "
       ]
-    }
     end
   end
 
   describe "overwrite" do
     test "overwrite first player" do
-      new_board = Board.new()
+      new_board =
+        Board.new()
         |> Board.play(Player.new(:x, 1), 1)
         |> Board.play(Player.new(:o, 2), 1)
 
-      assert new_board == %Board{
-        cells: [
-          :o, " ", " "," ",
-          " ", " ", " "," ",
-          " ", " ", " "," ",
-          " ", " ", " "," ",
-        ]
-      }
+      assert new_board.cells == [
+        :o, " ", " ", " ",
+        " ", " ", " ", " ",
+        " ", " ", " ", " ",
+        " ", " ", " ", " "
+      ]
     end
   end
 
   describe "revenge" do
     test "player2 cannot overwrite player1 to his previous place" do
-      new_board = Board.new()
+      new_board =
+        Board.new()
         |> Board.play(Player.new(:x, 1), 1)
         |> Board.play(Player.new(:o, 2), 1)
         |> Board.play(Player.new(:+, 3), 3)
 
-     result = Board.play(new_board, Player.new(:x, 1), 1) 
-     assert result == new_board 
+      result = Board.play(new_board, Player.new(:x, 1), 1)
+
+      assert result.cells == new_board.cells
     end
   end
 end
