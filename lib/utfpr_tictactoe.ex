458060f3b4742edd174ab242ab079ffe391dea13
Felipe Ramos <felipenegrelleramos@gmail.com>
2025-10-28 00:08:15 -0300

    [MAIN] adding main function - incomplete

--- lib/utfpr_tictactoe.ex
+++ lib/utfpr_tictactoe.ex
@@ -1,18 +1,18 @@
 defmodule UtfprTictactoe do
-  @moduledoc """
-  Documentation for `UtfprTictactoe`.
-  """
+  
+  def start_game() do
+    player1_symbol = IO.gets("Player 1 symbol: ") |> String.trim()
+    player1 = Player.new(player1_symbol, 1)
+    
+    player2_symbol = IO.gets("Player 2 symbol: ") |> String.trim()
+    player2 = Player.new(player2_symbol, 2)
 
-  @doc """
-  Hello world.
+    player3_symbol = IO.gets("Player 3 symbol: ") |> String.trim()
+    player3 = Player.new(player3_symbol, 3)
 
-  ## Examples
-
-      iex> UtfprTictactoe.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    board = Board.new()
   end
+
 end
+
+UtfprTictactoe.start_game()
